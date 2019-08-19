2581367cbe37c74e6921e243442bcf6ccc8bd523
Terrence <ter-lou@live.nl>
2019-08-19 19:34:52 +0200

    Home page uiterlijk wijziging, finetuning blogs pagina

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -5,57 +5,57 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   <script src="{{ asset('js/app.js') }}" defer></script>
-  <title>Be fit by ash - Blogs</title>
+  <title>Be fit By Ash - Blogs</title>
 </head>
-<body style="background-color: #cfdcd3 !important">
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark" style="background-color: #cfdcd3">
-    <a class="navbar-brand" href="#">Be fit By Ash</a>
+<body class="mb-5" style="background-color: #cfdcd3 !important">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-lg mb-3">
+    <a class="navbar-brand" href="/" style="font-style: italic; font-size: 1.7em; color: #cfdcd3 !important; font-weight: bold">Be Fit by Ash</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon "></span>
     </button>
-    <div class="collapse navbar-collapse center" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav" style="font-size: 1.2em;">
+        <li class="nav-item pr-4 pl-4">
+          <a class="nav-link stretched-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item active">
+        <li class="nav-item pr-4 pl-4 active">
           <a class="nav-link" href="/blogs">Blogs</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item pr-4 pl-4">
           <a class="nav-link" href="/tarieven">Tarieven</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item pr-4 pl-4 ">
           <a class="nav-link" href="#">Contact</a>
         </li>
       </ul>
     </div>
   </nav>
-  <main>
-    <div class="container">
-      <div id="list-blog" class="col-md-3 float-left mt-3 sticky-top">
-        <div class="list-group">
-          <img src="/img/logo.png" alt="logo" class="pb-2 center" style="width: 75%; height: auto; margin: 0 auto;">
-          @foreach ($blog as $blog1)
-            <a class="list-group-item list-group-item-action" href="#{{$blog1 -> name}}">{{$blog1 -> name}}</a>
-          @endforeach
-        </div>
-      </div>
-      <div data-spy="scroll" data-target="#list-blog" data-offset="0" class="scrollspy">
-        @foreach ($blog as $blog)
-        <div id="{{$blog -> name}}" class="col-md-9 float-right mt-3 ">
-          <div class="card">
-            <div class="card-header">
-              <h4 class="mt-1">{{$blog -> name}}</h4>
-              <p class="card-subtitle text-muted mb-0">Datum: 16/8/2019</p>
-            </div>
-            <div class="card-body">
-              {!!$blog -> blog!!}
-            </div>
+  <main role="main" class="container">
+    <div id="list-blog" class="col-md-3 float-left mt-3">
+      <img src="/img/logo.png" alt="logo" class="pb-4" style="width: 75%; height: auto; margin: 0 auto; display: block">
+      <ul class = "list-group shadow">
+        <h2 class="list-group-item bg-dark text-white">Blogs</h2>
+        @foreach ($blog as $blog1)
+          <a class="list-group-item list-group-item-action bg-dark text-white" href="#{{$blog1 -> name}}">{{$blog1 -> name}}</a>
+        @endforeach
+      </ul>
+    </div>
+    <div data-spy="scroll" data-target="#list-blog" data-offset="100" class="scrollspy">
+      @foreach ($blog as $blog)
+      <div id="{{$blog -> name}}" class="col-md-9 float-right mt-3">
+        <div class="card border-0 shadow">
+          <div class="card-header bg-dark text-white">
+            <h4 class="mt-1">{{$blog -> name}}</h4>
+            <p class="card-subtitle text-muted mb-0">Datum: 16/8/2019</p>
+          </div>
+          <div class="card-body">
+            {!!$blog -> blog!!}
           </div>
         </div>
-        @endforeach
       </div>
+      @endforeach
     </div>
   </main>
 </body>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,31 +4,51 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Be fit by ash</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <title>Be fit By Ash</title>
 </head>
-<body>
-  <header>
-    <div class="navbar">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/blogs">Blog</a></li>
-        <li><a href="/tarieven">Tarieven</a></li>
-        <li><a href="#contact">Contact</a></li>
+<body class="mb-5" style="background-color: #cfdcd3 !important">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-lg mb-3">
+    <a class="navbar-brand" href="/" style="font-style: italic; font-size: 1.7em; color: #cfdcd3 !important; font-weight: bold">Be Fit by Ash</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav" style="font-size: 1.2em;">
+        <li class="nav-item pr-4 pl-4 active">
+          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item pr-4 pl-4">
+          <a class="nav-link" href="/blogs">Blogs</a>
+        </li>
+        <li class="nav-item pr-4 pl-4">
+          <a class="nav-link" href="/tarieven">Tarieven</a>
+        </li>
+        <li class="nav-item pr-4 pl-4 ">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
       </ul>
     </div>
-  </header>
+  </nav>
   <main>
-    <img class="foto-home" src="/img/logo-min.png" alt="Foto van ashley">
-    <div class="wide-main dark">
-      <div class="wide-main-container">
-        <p>Mijn naam is Ashley van den Berg en oprichtster van Be fit by Ash. Mijn liefde voor sport zit er van jongs af aan al in. Tijdens mijn 4 jarige opleiding aan het CIOS heb ik mij verdiept in het menselijk lichaam, personal training, fitness en voeding.</p>
-        <p>Momenteel ben ik werkzaam bij een kinderopvang waar ik met veel plezier sportles geef! De andere dagen ben ik te vinden bij Doe fysiotherapie in Nieuw-Vennep. Hier mag ik het uiterste uit mensen halen. Er word hard getraind, maar ook is er altijd tijd voor een praatje!</p>
-        <p>Be fit by Ash coacht mensen naar een gezonder leefstijl! Succesvol afvallen bereik je vooral met de juiste mindset en een plan die past bij jou. Ga voor de beste versie van jezelf en zet jezelf is op de 1e plaats. Eis geen perfectie en geef jezelf de tijd!</p>
-        <p>Lekker eten? Normaal eten? Veel eten? Af en toe snoepen? En vooral blijven genieten van eten? JA DAT KAN!!</p>
+    <div class="container">
+      <img class="pb-3 pt-4" src="/img/logo-min.png" alt="Foto van ashley" width="40%" style="margin: 0 auto; display: block">
+      <div class="card">
+        <div class="card-body text-white bg-dark border-0">
+          <p>Mijn naam is Ashley van den Berg en oprichtster van Be fit by Ash. Mijn liefde voor sport zit er van jongs af aan al in. Tijdens mijn 4 jarige opleiding aan het CIOS heb ik mij verdiept in het menselijk lichaam, personal training, fitness en voeding.</p>
+          <p>Momenteel ben ik werkzaam bij een kinderopvang waar ik met veel plezier sportles geef! De andere dagen ben ik te vinden bij Doe fysiotherapie in Nieuw-Vennep. Hier mag ik het uiterste uit mensen halen. Er word hard getraind, maar ook is er altijd tijd voor een praatje!</p>
+          <p>Be fit by Ash coacht mensen naar een gezonder leefstijl! Succesvol afvallen bereik je vooral met de juiste mindset en een plan die past bij jou. Ga voor de beste versie van jezelf en zet jezelf is op de 1e plaats. Eis geen perfectie en geef jezelf de tijd!</p>
+          <p>Lekker eten? Normaal eten? Veel eten? Af en toe snoepen? En vooral blijven genieten van eten? JA DAT KAN!!</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img class="w-50" src="/img/foto-ashley.jpg">
+        </div>
       </div>
     </div>
-    <img class="foto-home" src="/img/foto-ashley.jpg" alt="">
   </main>
 </body>
 </html>
